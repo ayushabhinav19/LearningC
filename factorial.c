81cbffe9849a6e796e75a2431d12ac49ffac6761
Abhinav <ayushabhinav19@gmail.com>
2024-08-15 20:34:32 +0530

    Factorial Code Added

---
+++ factorial.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int factorial(int num,int i){
+    int fact;
+    printf("factorizing......\n");
+    fact=1;
+    for (i=1;i<=num;i++){
+        fact=fact*i;
+    }
+    return fact;
+}
+int main(){
+    int fact,number,j;
+    printf("enter the number= ");
+    scanf("%d",&number);
+    fact=factorial(number,j);
+    printf("the factorial of %d is %d",number,fact);
+    return 0;
+}
+
+//U
+//A 
+//M
